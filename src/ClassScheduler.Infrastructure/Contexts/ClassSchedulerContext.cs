e4d6cbe5de6e12b0f72029c4fd64815a27d96d64
oleges <oleges@list.ru>
2022-01-25 14:44:33 +0100

    Add Infrastructure project

---
+++ src/ClassScheduler.Infrastructure/Contexts/ClassSchedulerContext.cs
@@ -0,0 +1,20 @@
+﻿using ClassScheduler.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassScheduler.Infrastructure.Contexts
+{
+    public class ClassSchedulerContext : DbContext
+    {
+        public ClassSchedulerContext(DbContextOptions<ClassSchedulerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<LessonException> LessonExceptions { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
